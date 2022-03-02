572c4b765f62fae1a23581ddc70f92b5e887c4cd
kotschroedingera <79150323671@ya.ru>
2022-03-02 13:49:40 +0300

    added simple gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,46 @@
-const gulp = require('gulp');
-const pug2html = require('./src/gulp/tasks/pug2html');
+const { src, dest, task, watch, series } = require('gulp');
 
-module.exports.start = gulp.series(pug2html);
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const sync = require('browser-sync').create();
+
+
+function html() {
+  return src('./src/pages/**.pug')
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
+    .pipe(dest('./build'))
+    .pipe(sync.stream());
+}
+
+function css() {
+  return src('./src/styles/**.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./build/css'))
+    .pipe(sync.stream());
+}
+
+async function clear() {
+  await del(['./build']);
+}
+
+function serve() {
+  sync.init({
+    server: './build',
+  });
+
+  watch('./src/pages/**.pug', series(html)).on('change', sync.reload);
+  watch('./src/styles/**.scss', series(css)).on('change', sync.reload);
+}
+
+exports.html = html;
+exports.css = css;
+exports.clear = clear;
+exports.build = series(clear, css, html);
+exports.serve = series(clear, css, html, serve);
+
+
--- src/gulp/tasks/pug2html.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-
-module.exports = function pug2html(cb) {
-  return gulp.src('src/pages/**/*.pug')
-    .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest('build'))
-}
