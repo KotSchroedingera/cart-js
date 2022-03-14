eb4c1c12aa958ec1043a190472e8cb3b50c05f2c
kotschroedingera <79150323671@ya.ru>
2022-03-14 17:48:36 +0300

    1

---
+++ src/scripts/index.js
@@ -0,0 +1,4 @@
+import Select from "./select";
+
+const select = new Select(document.getElementsByClassName('form__select')[0]);
+select.showElem();
---
+++ src/scripts/select.js
@@ -0,0 +1,12 @@
+class Select {
+  constructor(elem) {
+    this.elem = elem;
+  }
+
+  showElem() {
+    console.log(this.elem);
+  }
+}
+
+
+export default Select;
