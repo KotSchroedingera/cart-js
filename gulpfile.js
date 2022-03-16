1393528324f3f77011ad6b105404f13f4e22c7b5
kotschroedingera <79150323671@ya.ru>
2022-03-16 17:49:11 +0300

    1

--- gulpfile.js
+++ gulpfile.js
@@ -78,11 +78,8 @@ export const js = () => {
           {
             test: /\.s[ac]ss$/i,
             use: [
-              // Creates `style` nodes from JS strings
               "style-loader",
-              // Translates CSS into CommonJS
               "css-loader",
-              // Compiles Sass to CSS
               "sass-loader",
             ],
           },
