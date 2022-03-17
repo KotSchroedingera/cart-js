22e4fa30a090343840babc05617b3ac1ef07ae29
kotschroedingera <79150323671@ya.ru>
2022-03-17 14:19:19 +0300

    1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,9 @@
 import Select from "./select/select.js";
 
-const select = new Select(document.getElementsByClassName('form__select')[0]);
-select.showElem();
+const select = new Select(
+  document.getElementsByClassName('form__select')[0], 
+  {
+    placeholder: 'Когда позвонить?'
+  }
+);
+select.render();
--- src/scripts/select/select.js
+++ src/scripts/select/select.js
@@ -1,12 +1,54 @@
+import './fa.js';
 import './styles.scss';
 
+const getHtml = (params = {}) => {
+  const placeholder = params.placeholder || 'Выберите зачение';
+
+  return `
+  <div class="select-js">
+    <div class="select-js__name">
+      <p>${placeholder}</p>
+      <i class="fa-solid fa-angle-down"></i>
+    </div>
+    <div class="select-js__list">
+      <ul>
+        <li data-value="any">Любое время</li>
+        <li data-value="9-12">9:00 — 12:00</li>
+        <li data-value="12-18">12:00 — 18:00</li>
+        <li data-value="18-21">18:00 — 21:00</li>
+      </ul>
+    </div>
+  `
+}
+
 class Select {
-  constructor(elem) {
+  constructor(elem, params) {
     this.elem = elem;
+    this.params = params;
   }
 
-  showElem() {
-    console.log(this.elem);
+  render() {
+    this.elem.innerHTML = getHtml(this.params);
+    this.elem.addEventListener('click', this.clickHandler.bind(this));
+  }
+
+  clickHandler(evt) {
+    console.log(evt.path);
+    this.toggle();
+  }
+
+  open() {
+    this.elem.querySelector('.select-js').classList.add('active');
+  }
+
+  close() {
+    this.elem.querySelector('.select-js').classList.remove('active');
+  }
+
+  toggle() {
+    this.elem.querySelector('.select-js').classList.contains('active') 
+      ? this.close()
+      : this.open()
   }
 }
 
